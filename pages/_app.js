335859fd3aa9d197382ce8faa2369011325db479
david gaston solache alvarez <a340583027@my.uvm.edu.mx>
2026-06-16 20:43:56 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,12 @@ import { useRouter } from "next/router";
 import { ChakraProvider, Container, Box } from "@chakra-ui/react";
 import theme from "../theme";
 import "../styles/globals.css";
-
 const App = ({ Component, pageProps }) => {
 	const { pathname } = useRouter();
 	const lang = pathname.startsWith("/es") ? "es" : "en";
-
 	useEffect(() => {
 		document.documentElement.lang = lang;
 	}, [lang]);
-
 	return (
 		<ChakraProvider theme={theme}>
 			<Box p={4}>
@@ -20,7 +17,7 @@ const App = ({ Component, pageProps }) => {
 					p={4}
 					boxShadow="md"
 					borderRadius="xl"
-					backgroundColor="primary.50"
+					backgroundColor="#FAF7F2"
 					color="#222"
 				>
 					<Component {...pageProps} />
@@ -29,5 +26,4 @@ const App = ({ Component, pageProps }) => {
 		</ChakraProvider>
 	);
 };
-
 export default App;
